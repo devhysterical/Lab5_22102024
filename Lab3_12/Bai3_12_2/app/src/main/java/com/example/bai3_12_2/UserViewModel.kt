67bbf1de06cfb3f99e7ce5ef1f4fb3dd6a0ed1fc
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2024-10-22 15:25:26 +0700

    update UserViewModel.kt

--- Lab3_12/Bai3_12_2/app/src/main/java/com/example/bai3_12_2/UserViewModel.kt
+++ Lab3_12/Bai3_12_2/app/src/main/java/com/example/bai3_12_2/UserViewModel.kt
@@ -3,6 +3,8 @@ package com.example.bai3_12_2
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class UserViewModel : ViewModel() {
     private val _users = MutableLiveData<List<User>>()
